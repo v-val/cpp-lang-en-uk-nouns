2ed420c0b890f78e1468b822394ab9c0a4202d04
Valerii Valeev <v@valeev.net>
2026-04-07 18:45:36 +0400

    Code exposing nouns as vector of string views. Build shared / static libraries.

---
+++ lang/en/uk/nouns.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Valerii Valeev on 4/7/26.
+//
+#include <regex>
+
+#include "nouns.hpp"
+
+
+extern "C" {
+    extern const char* _binary_lang_en_uk_nouns_txt_start;
+    extern const char* _binary_lang_en_uk_nouns_txt_end;
+    extern unsigned int _binary_lang_en_uk_nouns_txt_size;
+}
+
+namespace lang::en::uk {
+
+
+const std::vector<std::string_view> NOUNS = [] {
+    static const auto n = _binary_lang_en_uk_nouns_txt_end-_binary_lang_en_uk_nouns_txt_start;
+    static const std::regex WORD{"\\S+"};
+    std::vector<std::string_view> r{};
+    auto b = _binary_lang_en_uk_nouns_txt_start;
+    for ( std::cmatch m; std::regex_search(b, _binary_lang_en_uk_nouns_txt_end, m, WORD) ; b = m[0].second )
+        r.emplace_back(m[0].first, m[0].length());
+    return r;
+}();
+
+
+} // namespace lang::en::uk
---
+++ lang/en/uk/nouns.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Valerii Valeev on 4/7/26.
+//
+#ifndef LANG_EN_UK_NOUNS_HPP
+#define LANG_EN_UK_NOUNS_HPP
+
+#include <vector>
+#include <string_view>
+
+
+namespace lang::en::uk {
+
+extern const std::vector<std::string_view> NOUNS;
+
+} // namespace lang::en::uk
+#endif //LANG_EN_UK_NOUNS_HPP
